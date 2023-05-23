78c245fb79668162c09a43148faed7762e041209
Deftoner555 <jmoya555@gmail.com>
2023-05-22 23:36:36 -0700

    energy ball Destroyed with delay, still missing direction

--- Assets/Self/Scripts/Bullet.cs
+++ Assets/Self/Scripts/Bullet.cs
@@ -10,8 +10,8 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        //Destroy(effect, 5f);
-        Destroy(gameObject);
+        //Destroy(effect, 3f);
+        Destroy(gameObject, 3f);
     }
 
 }
